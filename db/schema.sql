68bd3fb4365aca0583078282b18b7e5992011593
Mark Heil <mark.heil1122@gmail.com>
2020-09-16 16:35:57 -0700

    created DB and schema/seeds, created config folder and connection.js

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE burgertime_db;
+USE burgertime_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+	name varchar
+    (255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY
+    (id)
+);
