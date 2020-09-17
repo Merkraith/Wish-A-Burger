d2d3d6b4e26a6d76d5e272af4c67c479efb3998a
Mark Heil <mark.heil1122@gmail.com>
2020-09-16 19:55:12 -0700

    create and edited burger.js in public/assets/js

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var devoured = $(this).data("newDevoured");
+  
+      var newDevourState = {
+        devour: devoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: devouredState
+      }).then(
+        function() {
+          console.log("changed devour to", devoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devour: $("[name=devour]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
