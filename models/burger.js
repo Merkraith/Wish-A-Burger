d1125a2a96b191fc02465d85cae0090d0832e07c
Mark Heil <mark.heil1122@gmail.com>
2020-09-16 19:01:13 -0700

    created models/burger.js

---
+++ models/burger.js
@@ -0,0 +1,28 @@
+let orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    createOne: function(cols, vals, cb) {
+      orm.createOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateONe("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    deleteOne: function(condition, cb) {
+      orm.deleteOne("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+  
+  // Export the database functions for the controller (catsController.js).
+  module.exports = burger;
